73de9bed15e4f8f877ee8d3cdd263df4e0378c29
niiyazobug <niiyaz5555@gmail.com>
2022-03-12 17:28:14 +0600

    some problem solved

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ input.addEventListener("keypress", function (event) {
 document.getElementById('error-message').style.display = 'none'
 
 const searchFood = () => {
-    
+
     const searchField = document.getElementById('search-field')
     const searchText = searchField.value
 
@@ -62,8 +62,7 @@ const displaySearchResult = (meals) => {
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title"> ${meal.strMeal} </h5>
-                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
-                
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p> 
             </div>
         </div>
         `
